089dd2737fc3b6c277a29a02edd90fb1bcc857d1
calisaurus <github@calisaurus.net>
2020-12-01 20:40:18 +0000

    solve day 1 part 1

---
+++ solutions/day1/solution.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const { read, position } = require('promise-path')
+const fromHere = position(__dirname)
+const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
+
+async function run () {
+  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+
+  await solveForFirstStar(input)
+  await solveForSecondStar(input)
+}
+
+async function solveForFirstStar (input) {
+  const solution = 'UNSOLVED'
+  const str = input
+  const res = str.split('\n').map(Number)
+
+  res.forEach(function (expense) {
+    const searchResult = res.filter(function (item) {
+      return item === 2020 - expense
+    })
+    console.log(searchResult)
+  })
+
+  report('Solution 1:', solution)
+}
+
+async function solveForSecondStar (input) {
+  const solution = 'UNSOLVED'
+  report('Solution 2:', solution)
+}
+
+run()

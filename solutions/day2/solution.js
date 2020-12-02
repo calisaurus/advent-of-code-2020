ef1ec9d4f449cb9745f49f41f92baf7d73d28c13
calisaurus <github@calisaurus.net>
2020-12-02 10:51:23 +0000

    Solve part 1

--- solutions/day2/solution.js
+++ solutions/day2/solution.js
@@ -4,7 +4,7 @@ const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
 
 async function run () {
-  const input = (await read(fromHere('testInput.txt'), 'utf8')).trim()
+  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
 
   await solveForFirstStar(input)
   await solveForSecondStar(input)
@@ -14,30 +14,40 @@ function findCountOfChar (a, b) {
   return a.split('').filter(x => x === b).length
 }
 
+function validatePasswordLine (passwordLine) {
+  const line = passwordLine.replace(':', '').replace('-', ' ')
+
+  const min = line.split(' ')[0]
+  const max = line.split(' ')[1]
+  const character = line.split(' ')[2]
+  const password = line.split(' ')[3]
+
+  const countOfChar = findCountOfChar(password, character)
+
+  if (countOfChar >= min && countOfChar <= max) {
+    return true
+  } else {
+    return false
+  }
+}
+
 async function solveForFirstStar (input) {
   const solution = 'UNSOLVED'
 
   const lines = input.split('\n')
 
-  const line = lines[0].replace(':', '').replace('-', ' ')
+  let result
+  let counter = 0
 
-  console.log(line)
+  lines.forEach(function (line) {
+    result = validatePasswordLine(line)
+    if (result === true) {
+      counter++
+    }
+  })
+  console.log('count = ', counter)
 
-  const password = line.split(' ')[3]
-  console.log(password)
-  const character = line.split(' ')[2]
-  console.log('Char', character)
-  const min = line.split(' ')[0]
-  const max = line.split(' ')[1]
-
-  const countOfChar = findCountOfChar(password, character)
-  console.log(countOfChar)
-
-  if (countOfChar >= min && countOfChar <= max) {
-    console.log(true)
-  }
-
-  report('Input:', input)
+  // report('Input:', input)
   report('Solution 1:', solution)
 }
 

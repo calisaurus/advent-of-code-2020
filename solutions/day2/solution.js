b3aceb44ca5caf14b57b2a1e566667bc1f8707df
calisaurus <github@calisaurus.net>
2020-12-02 21:11:06 +0000

    Solve part 2

--- solutions/day2/solution.js
+++ solutions/day2/solution.js
@@ -5,6 +5,7 @@ const report = (...messages) => console.log(`[${require(fromHere('../../package.
 
 async function run () {
   const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+  // const test = (await read(fromHere('testInput.txt'), 'utf8')).trim()
 
   await solveForFirstStar(input)
   await solveForSecondStar(input)
@@ -12,6 +13,10 @@ async function run () {
 
 function findCountOfChar (a, b) {
   return a.split('').filter(x => x === b).length
+}
+
+function getCharacterAtPosition (str, position) {
+  return str.split('')[position - 1]
 }
 
 function validatePasswordLine (passwordLine) {
@@ -31,9 +36,28 @@ function validatePasswordLine (passwordLine) {
   }
 }
 
-async function solveForFirstStar (input) {
-  const solution = 'UNSOLVED'
+function validatePasswordByPosition (passwordLine) {
+  const line = passwordLine.replace(':', '').replace('-', ' ')
 
+  const positionA = line.split(' ')[0]
+  const positionB = line.split(' ')[1]
+  const character = line.split(' ')[2]
+  const password = line.split(' ')[3]
+
+  const characterAtPositionA = getCharacterAtPosition(password, positionA)
+  const characterAtPositionB = getCharacterAtPosition(password, positionB)
+
+  const characterIsNotSame = characterAtPositionA !== characterAtPositionB
+  const characterExistsAtPosition = characterAtPositionA === character || characterAtPositionB === character
+
+  if (characterIsNotSame && characterExistsAtPosition) {
+    return true
+  } else {
+    return false
+  }
+}
+
+async function solveForFirstStar (input) {
   const lines = input.split('\n')
 
   let result
@@ -45,15 +69,22 @@ async function solveForFirstStar (input) {
       counter++
     }
   })
-  console.log('count = ', counter)
-
-  // report('Input:', input)
-  report('Solution 1:', solution)
+  report('Part 1 solution = ', counter)
 }
 
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
-  report('Solution 2:', solution)
+  const lines = input.split('\n')
+
+  let result
+  let counter = 0
+
+  lines.forEach(function (line) {
+    result = validatePasswordByPosition(line)
+    if (result === true) {
+      counter++
+    }
+  })
+  report('Part 2 solution =', counter)
 }
 
 run()

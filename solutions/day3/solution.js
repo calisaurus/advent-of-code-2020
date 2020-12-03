44013bfc2eec6f753594f699d5d1064ddc2cfac4
calisaurus <github@calisaurus.net>
2020-12-03 23:26:49 +0000

    Solve day 3 part 2

--- solutions/day3/solution.js
+++ solutions/day3/solution.js
@@ -16,6 +16,26 @@ function getTileAt (lines, x, y) {
   return lines[y].charAt(x % width)
 }
 
+function testASlope (input, dx, dy) {
+  const lines = input.split('\n')
+  const height = lines.length
+  let posx = 0
+  let posy = 0
+
+  let counter = 0
+
+  do {
+    const character = getTileAt(lines, posx, posy)
+    if (character === '#') {
+      counter++
+    }
+    posx = posx + dx
+    posy = posy + dy
+  } while (posy < height)
+
+  return counter
+}
+
 async function solveForFirstStar (input) {
   const dx = 3
   const dy = 1
@@ -40,7 +60,15 @@ async function solveForFirstStar (input) {
 }
 
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const a = testASlope(input, 1, 1)
+  const b = testASlope(input, 3, 1)
+  const c = testASlope(input, 5, 1)
+  const d = testASlope(input, 7, 1)
+  const e = testASlope(input, 1, 2)
+
+  console.log(a, b, c, d, e)
+
+  const solution = a * b * c * d * e
   report('Solution 2:', solution)
 }
 
